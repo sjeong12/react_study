d1fd3f86f3844815dfb0774d3a880de675089bc9
sujlee <sujlee@student.42seoul.kr>
2021-05-10 15:13:12 +0900

    [feat] remove

---
+++ phone_book/src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import React, {Component} from 'react';
+import PhoneForm from './components/PhoneForm.js';
+import PhoneInfoList from './components/PhoneInfoList.js';
+
+class App extends Component {
+	id = 2
+	state = {
+		information: [
+			{
+				id: 0,
+				name: '김가나',
+				phone: '010-0000-0000'
+			},
+			{
+				id: 1,
+				name: '김다라',
+				phone: '010-1111-1111'
+			}
+		]
+	}
+
+	handleCreate = (data) => {
+		const {information} = this.state;
+		this.setState({
+			information: information.concat({id: this.id++, ...data})
+		})
+	}
+
+	handleRemove = (id) => {
+		const {information} = this.state;
+		this.setState({
+			information: information.filter(info => info.id !== id)
+		})
+	}
+
+	render() {
+		const {information} = this.state;
+		return (
+			<div className="App">
+				<PhoneForm
+					onCreate={this.handleCreate}
+				/>
+				<PhoneInfoList
+					data={information}
+					onRemove={this.handleRemove}
+				/>
+			</div>
+		);
+	}
+}
+export default App;
